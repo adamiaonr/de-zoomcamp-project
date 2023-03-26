6e5bf099854f704ab4c61cf1c99842d8561c7825
Antonio Rodrigues <adamiaonr@gmail.com>
2023-03-26 23:19:18 +0100

    modified elt flow to gather Kaggle credentials from secret and set them as env variables within the script

--- workflow/orchestration/elt_kaggle_to_gcs.py
+++ workflow/orchestration/elt_kaggle_to_gcs.py
@@ -3,6 +3,7 @@ from pathlib import Path
 from kaggle.api.kaggle_api_extended import KaggleApi  # pylint: disable-msg=E0611
 from prefect import flow, task
 from prefect_gcp.cloud_storage import GcsBucket
+from prefect.blocks.system import Secret
 
 def get_kaggle_client() -> KaggleApi:
   """
@@ -31,18 +32,21 @@ def create_local_dir() -> Path:
   return path
 
 @task(retries=3)
-def download_from_kaggle(month: int, output_dir: Path) -> Path:
+def download_from_kaggle(month: int, output_dir: Path, kaggle_dataset_id: str) -> Path:
   """
   Downloads the dataset file for the specified month from Kaggle into local path.
   Returns path of saved file.
   Data is downloaded in compressed .zip format.
   """
-  kaggle_id = os.getenv(
-    'KAGGLE_DATASET_ID', 'stoney71/new-york-city-transport-statistics'
-  )
+  # fetch kaggle API user and key from secret block
+  username, key = fetch_kaggle_credentials()
+  # set kaggle credentials as env variables
+  set_kaggle_credentials(username, key)
+  
+  # download dataset file for month
   file_path = f"mta_17{int(month):02d}.csv"
   get_kaggle_client().dataset_download_file(
-    kaggle_id,
+    kaggle_dataset_id,
     file_name=file_path,
     path=output_dir,
     force=False
@@ -50,8 +54,24 @@ def download_from_kaggle(month: int, output_dir: Path) -> Path:
 
   return output_dir/f"{file_path}.zip"
 
+def fetch_kaggle_credentials() -> tuple[str]:
+  """Fetches Kaggle API credentials from secret block"""
+  kaggle_credentials_block = os.getenv('PREFECT_KAGGLE_CREDENTIALS_BLOCK')
+  secret_block = Secret.load(kaggle_credentials_block)
+  kaggle_credentials = secret_block.get()
+
+  return kaggle_credentials.split(',')
+
+def set_kaggle_credentials(username: str, key: str) -> None:
+  """Sets passed Kaggle API username and key as environment variables"""
+  os.environ['KAGGLE_USERNAME'] = username
+  os.environ['KAGGLE_KEY'] = key
+
 @flow()
-def elt_main_flow(months: list[int] = [6]) -> None:
+def elt_main_flow(
+  months: list[int] = [6],
+  kaggle_dataset_id: str = 'stoney71/new-york-city-transport-statistics'
+) -> None:
   """
   Given a list of months: 
     1. Downloads dataset files from Kaggle, one for each month
